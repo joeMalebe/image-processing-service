ee9820312ef89e429c4d1dc42fdf9cec6bd8c99d
Joe Malebe <Joe.Malebe@standardbank.co.za>
2026-03-14 09:08:00 +0200

    initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.cio.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.default.headers)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
