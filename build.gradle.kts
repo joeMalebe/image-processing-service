92c56ef2efdf59b1d84a2dc72eb105379baa3309
Joe Malebe <Joe.Malebe@standardbank.co.za>
2026-03-19 17:38:29 +0200

    add exposed library for db communication

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,10 @@ dependencies {
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.server.auth)
     implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.mysql)
+    implementation(libs.exposed.dao)
 
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
