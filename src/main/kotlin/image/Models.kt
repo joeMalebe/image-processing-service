9d71183f992b1b5f1a740b6968b99f33b42513cf
Joe Malebe <Joe.Malebe@standardbank.co.za>
2026-03-24 19:03:56 +0200

    create model for image formatting request

---
+++ src/main/kotlin/image/Models.kt
@@ -0,0 +1,26 @@
+package com.example.image
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ImageFormattingRequest(val resize: Resize? = null,
+                           val crop: Crop? = null,
+                           val rotate: Rotate? = null,
+                           val format: Format? = null
+)
+
+@Serializable
+data class Resize(val width: Int, val height: Int)
+
+@Serializable
+data class Crop(val width: Int, val height: Int, val x: Int, val y: Int)
+
+@Serializable
+data class Rotate(val angle: Int)
+
+@Serializable
+data class Format(val type: String)
+
+@Serializable
+data class Filters(val dihter: Boolean = false)
+
