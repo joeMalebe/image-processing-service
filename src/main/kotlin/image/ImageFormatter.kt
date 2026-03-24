e7ba0e50d2a6ec9f67e1e8cb8235201689321b48
Joe Malebe <Joe.Malebe@standardbank.co.za>
2026-03-24 20:02:06 +0200

    create image formatting delegate

---
+++ src/main/kotlin/image/ImageFormatter.kt
@@ -0,0 +1,19 @@
+package com.example.image
+
+import com.sksamuel.scrimage.nio.ImageSource
+import com.sksamuel.scrimage.nio.ImmutableImageLoader
+import com.sksamuel.scrimage.nio.InputStreamImageSource
+import com.sksamuel.scrimage.nio.JpegWriter
+
+class ImageFormatter {
+    fun formatImage(
+        inputFile: ByteArray,
+        formatting: ImageFormattingRequest = ImageFormattingRequest()
+    ): ByteArray {
+        if (formatting.resize == null) return inputFile
+        val result = ImmutableImageLoader().fromBytes(inputFile)
+            .resizeTo(formatting.resize.width, formatting.resize.height)
+        ImageSource.of(inputFile.inputStream())
+        return result.bytes(JpegWriter())
+    }
+}
